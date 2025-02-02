52b0eefac759ef1e6a9efc97ee818f7bf35bd76d
AlexanderSZT <alexandresauzet2@gmail.com>
2025-02-02 19:46:48 +0100

    chore: changed port from 3000 to 3001

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { stationsRouter } from "./routes/stations";
 import { rollingStockRouter } from "./routes/rollingStock";
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(morgan("dev"));
 
